071f7c4c40b15989de06754d1c25872cf0c6d534
Lucas Rodriguez <lrodriguez@lexim.solutions.com>
2023-07-13 21:18:33 -0300

    Quinto Commit

--- parcial_juego/gui_form_menu_F.py
+++ parcial_juego/gui_form_menu_F.py
@@ -12,10 +12,10 @@ class FormMenuF(Form):
     def __init__(self,name,master_surface,x,y,w,h,image_background,active):
         super().__init__(name,master_surface,x,y,w,h,image_background,active)
 
-        self.boton1 = Button(master=self,x=0,y=0,w=140,h=50,color_background=None,color_border=None,image_background="parcial_juego/images/gui/set_gui_01/Comic_Border/Buttons/Button_M_02.png",on_click=self.on_click_boton,on_click_param="form_menu_A",text="MENU",font="Verdana",font_size=30,font_color=C_WHITE)        
+        self.boton1 = Button(master=self,x=100,y=250,w=140,h=50,color_background=None,color_border=None,image_background="parcial_juego/images/gui/set_gui_01/Comic_Border/Buttons/Button_M_02.png",on_click=self.on_click_boton,on_click_param="form_menu_A",text="MENU",font="Verdana",font_size=30,font_color=C_WHITE)        
 
         self.lista_widget = [self.boton1]
-        
+
     def on_click_boton(self, parametro):
         self.set_active(parametro)
 
